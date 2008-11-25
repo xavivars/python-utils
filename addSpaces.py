49879d1b06430d5025e9312910a55bee2c8e3e6c
Xavi Ivars <xavi.ivars@gmail.com>
2008-11-25 11:18:33 +0000

    FIXED: Bug UTF
    git-svn-id: svn+ssh://infobenissa.com/home/xavivars/svn/pythonutils@3 862c99d9-a65b-0410-bd2e-8650ab2594aa

--- addSpaces.py
+++ addSpaces.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python
-# -*- coding: utf-8 -*-
+# coding=utf-8
+# -*- encoding: utf-8 -*-
 
 # Programa que separa les lletres d'una paraula amb espais
 
-import sys
-import codecs
+#sys.setdefaultencoding('utf-8');
 
-# indiquem la codifiacio de stdin
-sys.stdin = codecs.getreader("utf-8")(sys.stdin)
+
+import sys, codecs, copy;
+
+sys.stdin  = codecs.getreader('utf-8')(sys.stdin);
+sys.stdout = codecs.getwriter('utf-8')(sys.stdout);
+sys.stderr = codecs.getwriter('utf-8')(sys.stderr);
 
 j=0
 
@@ -23,6 +27,8 @@ for linea in sys.stdin:
 
 	# l'encode és necessari perquè python té un bug
 	# http://drj11.wordpress.com/2007/05/14/python-how-is-sysstdoutencoding-chosen/#comment-3503
-	print b.encode('utf-8');
+	# print b.encode('utf-8');
+	# també es soluciona posant el getwriter del principi
+	print b;
 
 
