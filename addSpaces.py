4b0f5007087c0f7bc057eb3f4be33d85491fbc0b
Xavi Ivars <xavi.ivars@gmail.com>
2008-11-25 09:32:04 +0000

    arreglat bug en l'eixida
    git-svn-id: svn+ssh://infobenissa.com/home/xavivars/svn/pythonutils@2 862c99d9-a65b-0410-bd2e-8650ab2594aa

--- addSpaces.py
+++ addSpaces.py
@@ -9,12 +9,20 @@ import codecs
 # indiquem la codifiacio de stdin
 sys.stdin = codecs.getreader("utf-8")(sys.stdin)
 
+j=0
+
 # llegim l'entrada estandar
 for linea in sys.stdin:
+	j=j+1;
+
 	b=linea[0];
 	# cal fer-ho fins a len(linea)-1 per a que no tinga
 	# en compte el propi final de linia
 	for i in range(1,len(linea)-1):
 		b+=' '+linea[i];
-	print b;
+
+	# l'encode és necessari perquè python té un bug
+	# http://drj11.wordpress.com/2007/05/14/python-how-is-sysstdoutencoding-chosen/#comment-3503
+	print b.encode('utf-8');
+
 
